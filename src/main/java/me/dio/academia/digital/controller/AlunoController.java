40331a027fa77a64ac294c0d77021f8cf5100eb1
Raul Coelho <raulpereira5@gmail.com>
2023-04-26 16:45:39 -0300

    alunos completo

--- src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -3,17 +3,18 @@ package me.dio.academia.digital.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.dio.academia.digital.entity.Aluno;
 import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.entity.form.AlunoUpdateForm;
 import me.dio.academia.digital.service.impl.AlunoServiceImpl;
 
 @RestController
@@ -41,6 +42,11 @@ public class AlunoController {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         service.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public Aluno update(@PathVariable Long id, @RequestBody AlunoUpdateForm form) {
+        return service.update(id, form);
     }
 
 }
--- src/main/java/me/dio/academia/digital/repository/AlunoRepository.java
+++ src/main/java/me/dio/academia/digital/repository/AlunoRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import me.dio.academia.digital.entity.Aluno;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long>{
+    
 }
